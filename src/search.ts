9de175cee82ba8e251381f1ef5d5b9f2a602919b
Pablo Oliva <p.o@pablooliva.de>
2022-09-19 06:58:32 +0200

    Improve CSE params definition. Add GSCE annotations TSV file.

--- src/search.ts
+++ src/search.ts
@@ -1,13 +1,40 @@
+interface CSEIdParams {
+	[key: string]: string;
+}
+
+interface CustomSearchEngines {
+	[key: string]: {
+		label: string;
+		searchIdParams: CSEIdParams;
+	};
+}
+
 export class Search {
 	private _buildUrl(searchTerms: string): URL {
+		const extractCSEIdParams = (cse: CustomSearchEngines): CSEIdParams => {
+			let params: CSEIdParams = {};
+			Object.values(cse).forEach((val) => {
+				params = { ...params, ...val.searchIdParams };
+			});
+
+			return params;
+		};
+
 		const baseUrl = 'https://cse.google.com/cse';
-		const customSearchEngineParams = {
-			cx: '838e1bad8dae94387',
-		} as const;
+		const customSearchEngines: CustomSearchEngines = {
+			'ng-dev': {
+				label: 'Angular development',
+				searchIdParams: {
+					cx: '838e1bad8dae94387',
+				},
+			},
+		};
 
 		// Ex: 'https://cse.google.com/cse?cx=838e1bad8dae94387#gsc.q=testing router angular'
 		let url = new URL(baseUrl);
-		url = this._urlEncodeParams(url, customSearchEngineParams);
+		url = this._urlEncodeParams(url, {
+			...extractCSEIdParams(customSearchEngines),
+		});
 		url = this._addSearchTerms(url, searchTerms);
 
 		return url;
